b030547146f0f9b04cb50e70bc6fae278162d8b7
Jéssica Silva <jessica.akyla@gmail.com>
2024-09-30 19:54:29 -0300

    Add comentarios e alterando o host para ficar o mesmo para todos.

--- db.py
+++ db.py
@@ -2,10 +2,12 @@ import mysql.connector
 from mysql.connector import Error
 
 # Conectando ao banco de dados
+# Antes faça o passo a passo no worbench, está no grupo!
+# Crie sua branch 
 def conectarDb():
     try:
         conexao = mysql.connector.connect(
-            host='localhost',
+            host='192.168.1.14',        
             database="esperanca",
             user="dev",
             password="Esperanca@2024"
@@ -22,4 +24,9 @@ def desconectarDb(conexao):
     if conexao.is_connected():
         conexao.close()
         print("Desconectado com sucesso!")
+
+# Verifica a conexão, não vamos ficar 
+# usando assim isso é só pra ver no terminal que está funcionando.
+con = conectarDb()
+desc = desconectarDb(con)
         
